26ea53a829564e18c9f1eef6547e2b805687c1a3
Yasakatsu <chappieoct4@gmail.com>
2024-02-12 00:20:33 +0900

    👍Route設定追記 dashboard表示はloginユーザのみ

--- PHP/Laravel/test-project/routes/web.php
+++ PHP/Laravel/test-project/routes/web.php
@@ -22,7 +22,7 @@ Route::get('/test', [TestController::class, 'test'])
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->middleware(('auth'));
 
 Route::get('/dashboard', function () {
     return view('dashboard');
